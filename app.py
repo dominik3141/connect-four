247777cf3a95ca72c38f9c8cc4ddeabb8c678e21
Dominik Farr <dominik.farr@icloud.com>
2024-09-27 19:34:28 -0700

    Game state now saved on device. Therefore possible to handle multiple games at the same time.

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ from ai import (
 from minimax import minimax_move
 import torch
 import os
+import numpy as np
+from typing import List
 
 app = Flask(__name__)
 
-# Initialize the game state and model
-game = ConnectFour()
+# Initialize the model
 model = DecisionModel()
-model_path = "model_minimax.pth"
+model_path = "model.pth"
 
 # Load the trained model if available
 if os.path.exists(model_path):
@@ -25,8 +26,8 @@ else:
     pass
 
 
-def serialize_board(board: ConnectFour):
-    return board.state.tolist()
+def deserialize_board(board_state: List[List[int]]) -> ConnectFour:
+    return ConnectFour(np.array(board_state))
 
 
 @app.route("/")
@@ -36,19 +37,27 @@ def index():
 
 @app.route("/start", methods=["GET"])
 def start_game():
-    global game
     game = ConnectFour()
-    return jsonify({"board": serialize_board(game)})
+    return jsonify({"board": game.state.tolist()})
 
 
 @app.route("/move", methods=["POST"])
 def player_move():
-    global game
     data = request.get_json()
-    move = data.get("move")
+    board_state: List[List[int]] = data.get("board")
+    move: int = data.get("move")
+    opponent: str = data.get("opponent", "ai")
+    depth: int = data.get("depth", 1)
 
-    if move is None or not isinstance(move, int) or not (0 <= move <= 6):
-        return jsonify({"error": "Invalid move."}), 400
+    if (
+        not board_state
+        or move is None
+        or not isinstance(move, int)
+        or not (0 <= move <= 6)
+    ):
+        return jsonify({"error": "Invalid input."}), 400
+
+    game = deserialize_board(board_state)
 
     if not is_legal(game, move):
         return jsonify({"error": "Illegal move."}), 400
@@ -60,55 +69,21 @@ def player_move():
 
     status = is_in_terminal_state(game)
     if status != 0:
-        return jsonify({"board": serialize_board(game), "status": status})
+        return jsonify({"board": game.state.tolist(), "status": status})
 
-    return jsonify({"board": serialize_board(game), "status": status})
-
-
-@app.route("/ai_move", methods=["GET"])
-def ai_move():
-    global game, model
-    if is_in_terminal_state(game) != 0:
-        return jsonify(
-            {"board": serialize_board(game), "status": is_in_terminal_state(game)}
-        )
-
-    try:
-        move, prob = get_next_move(model, game)
-        game = make_move(game, 2, move)  # AI is player 2
-    except ValueError as e:
-        return jsonify({"error": str(e)}), 400
+    # AI's turn
+    if status == 0:
+        try:
+            if opponent == "minimax":
+                ai_move = minimax_move(game, depth)
+            else:
+                ai_move, _ = get_next_move(model, game)
+            game = make_move(game, 2, ai_move)  # AI is player 2
+        except ValueError as e:
+            return jsonify({"error": str(e)}), 400
 
     status = is_in_terminal_state(game)
-    return jsonify({"board": serialize_board(game), "status": status})
-
-
-@app.route("/minimax_move", methods=["POST"])
-def get_minimax_move():
-    global game
-    data = request.get_json()
-    depth = data.get("depth", 1)  # Default depth is 1 if not provided
-
-    if is_in_terminal_state(game) != 0:
-        return jsonify(
-            {"board": serialize_board(game), "status": is_in_terminal_state(game)}
-        )
-
-    try:
-        move = minimax_move(game, depth)
-        game = make_move(game, 2, move)  # AI is player 2
-    except ValueError as e:
-        return jsonify({"error": str(e)}), 400
-
-    status = is_in_terminal_state(game)
-    return jsonify({"board": serialize_board(game), "status": status})
-
-
-@app.route("/reset", methods=["GET"])
-def reset_game():
-    global game
-    game = ConnectFour()
-    return jsonify({"board": serialize_board(game)})
+    return jsonify({"board": game.state.tolist(), "status": status})
 
 
 if __name__ == "__main__":
